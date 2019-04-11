1cfd1ec2c15d5d7f9b60c2dbe53e49a88b3003c8
Jorik <jorikkornev@gmail.com>
2019-04-11 17:38:00 +0300

    test version 0.1

---
+++ source/js/tnsSettings.js
@@ -0,0 +1,54 @@
+
+let slider = tns({
+    container: '.slider-data-1',
+    items: 1,
+    responsive: {
+        640: {
+            edgePadding: 20,
+            gutter: 10,
+            items: 2
+        },
+        700: {
+            gutter: 30
+        },
+        900: {
+            items: 3,
+            edgePadding: 20,
+            gutter: 10,
+        }
+    }
+});
+let slider2 = tns({
+    container: '.slider-data-2',
+    items: 1,
+    responsive: {
+        640: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 2
+        },
+        700: {
+            gutter: 30
+        },
+        900: {
+            items: 2
+        }
+    }
+});
+let slider3 = tns({
+    container: '.slider-data-3',
+    items: 1,
+    responsive: {
+        640: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 2
+        },
+        700: {
+            gutter: 30
+        },
+        900: {
+            items: 2
+        }
+    }
+});
